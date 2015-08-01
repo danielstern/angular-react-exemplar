7552bd30701908cd574a09f997ee18d235bb3ddd
Code Whisperer <daniel@danielstern.ca>
2015-08-01 03:44:13 -0400

    implement repeating list

--- main.js
+++ main.js
@@ -3,11 +3,36 @@ angular.module("MailboxApp",['ui.router'])
     $urlRouterProvider.otherwise("/inbox");
     $stateProvider
     .state('inbox', {
-      url: "/inbox",
-      templateUrl: "partials/inbox.html"
-    })
-    
+        url: "/inbox",
+        templateUrl: "partials/inbox.html",
+        controller:function($scope,messageStore){
+            $scope.messages = messageStore.getMessages();
+        }
+    })   
 })
 .run(function(){
     console.info("Mailbox Running.");
+})
+.controller("MessageRowController",function($scope){
+    var g = Math.random();
+})
+.service("messageStore",function(){
+    var messages = [];
+    // at 100 it's fine
+    // at 1000 its a little buggy
+    // at 10000 its all over
+    var sampleSize = 100;
+    for (var i = 0; i < sampleSize; i++){
+        messages.push({
+            sender:`john.smith${i}@gmail.com`,
+            date:Date.now() - i*60000,
+            subject:`Regarding report DS-${i}`,
+            text:`Where's that report? I've been waiting ${i} days. -John`
+        })
+    };
+    return {
+        getMessages:function(){
+            return messages;
+        }
+    }
 })
