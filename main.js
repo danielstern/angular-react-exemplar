708a0fbeeea89823dbdede40a0d993db346da93c
Code Whisperer <daniel@danielstern.ca>
2015-08-01 04:20:37 -0400

    add inbox view + router

--- main.js
+++ main.js
@@ -9,22 +9,30 @@ angular.module("MailboxApp",['ui.router'])
             $scope.messages = messageStore.getMessages();
         }
     })   
-})
-.run(function(){
-    console.info("Mailbox Running.");
+    .state('message', {
+        url: "/message/:id",
+        templateUrl: "partials/message.html",
+        controller:function($scope,messageStore,$stateParams){
+            $scope.message = messageStore.getMessages().filter(function(m){
+                return m.id == $stateParams.id;
+            })[0];
+            
+        }
+    })   
 })
 .service("messageStore",function(){
     var messages = [];
     // at 100 it's fine
     // at 1000 its a little buggy
     // at 10000 its all over
-    var sampleSize = 10000;
+    var sampleSize = 1000;
     for (var i = 0; i < sampleSize; i++){
         messages.push({
             sender:`john.smith${i}@gmail.com`,
             date:Date.now() - i*60000,
+            id:i,
             subject:`Regarding report DS-${i}`,
-            text:`Where's that report? I've been waiting ${i} days. -John`
+            body:`Where's that report? I've been waiting ${i} days. -John`
         })
     };
     return {
